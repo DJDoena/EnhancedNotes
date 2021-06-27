d91a34dd1497e1d194623312d017f1cf5a4bc078
DJ Doena <github@doena-soft.de>
2021-06-27 10:18:11 +0200

    1.1.4.8

--- EnhancedNotes/EnhancedNotes/Properties/AssemblyInfo.cs
+++ EnhancedNotes/EnhancedNotes/Properties/AssemblyInfo.cs
@@ -6,14 +6,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Doena Soft.")]
 [assembly: AssemblyProduct("Enhanced Notes")]
-[assembly: AssemblyCopyright("Copyright © Doena Soft. 2015 - 2020")]
+[assembly: AssemblyCopyright("Copyright © Doena Soft. 2015 - 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(true)]
 [assembly: Guid("28399754-D633-4E03-93DB-4F56DAB6158C")]
 
-[assembly: AssemblyVersion("1.1.4.7")]
-[assembly: AssemblyFileVersion("1.1.4.7")]
+[assembly: AssemblyVersion("1.1.4.8")]
+[assembly: AssemblyFileVersion("1.1.4.8")]
 
 [assembly: AssemblyDelaySign(false)]
